8c648337a95d4e2cf304b65d4f436bf902dff41b
9beach <9beach@gmail.com>
2022-11-23 15:49:59 +0900

    Update misc

--- tests/t.sh
+++ tests/t.sh
@@ -1,11 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+# Temporary working directory
+DIR=$(mktemp -d)
 
 function assert_ok {
-	if [ $1 -eq 0 ]; then
-		echo \# ok: $2
+	cmd=$(echo $1 | sed -e 's:\.\./::g' -e 's:/var[^ ]*/::g' -e 's:/tmp[^ ]*/::g')
+	(( ${#cmd} > 72 )) && cmd="${cmd:0:69}..."
+	eval "$1"
+	if [ $? -eq 0 ]; then
+		echo "# ok:" $cmd
 	else
-		echo \# not ok: $2
-		rm -f $t1 $t2
+		echo "# not ok:" $cmd
+		rm -rf $DIR
 		exit 1
 	fi
 }
@@ -24,64 +30,58 @@ function chext {
 
 cd "$(dirname "$0")" > /dev/null 2>&1
 
-../srttidy -d 'lee.*ta' <s03-ascii.srt | diff - s03-ascii-d.out
-assert_ok $? "srttidy -d 'lee.*ta' <s03-ascii.srt"
+echo \# Checks SRTTIDY
+assert_ok "../srttidy -d 'lee.*ta' <s03-ascii.srt | diff - s03-ascii-d.out"
+assert_ok "../srttidy -d 'lee.*ta' <s03-ascii-bom-cr.srt | diff - s03-ascii-bom-cr-d.out"
+assert_ok "../srttidy -r -d 'lee.*ta' <s03-ascii-bom-cr.srt | diff - s03-ascii-bom-cr-d.out"
+assert_ok "../srttidy -m 2,0.1 <s03-ascii-bom-cr.srt 2> /dev/null | diff - s03-ascii-bom-cr-m.out"
+assert_ok "../srttidy -m 2,0.1 <s03-ascii.srt 2> /dev/null|diff - s03-ascii-m.out"
 
-../srttidy -d 'lee.*ta' <s03-ascii-bom-cr.srt | diff - s03-ascii-bom-cr-d.out
-assert_ok $? "srttidy -d 'lee.*ta' <s03-ascii-bom-cr.srt"
-
-# Checks if test fixtures are valid
-my_iconv cp949 s01-cp949.smi | diff - s01-utf8.smi && \
-	my_iconv utf-16 s01-utf16.smi | diff - s01-utf8.smi && \
-	my_iconv utf-16 s01-utf16.smi | diff - s01-utf8.smi && \
-	my_iconv cp949 s01-cp949.srt | diff - s01-utf8.srt && \
-	my_iconv utf-16 s01-utf16.srt | diff - s01-utf8.srt && \
-	my_iconv utf-16 s01-utf16.srt | diff - s01-utf8.srt && 
-	my_iconv cp949 s02-cp949.smi | diff - s02-utf8.smi && \
-	my_iconv utf-16 s02-utf16.smi | diff - s02-utf8.smi && \
-	my_iconv utf-16 s02-utf16.smi | diff - s02-utf8.smi && \
-	my_iconv cp949 s02-cp949.srt | diff - s02-utf8.srt && \
-	my_iconv utf-16 s02-utf16.srt | diff - s02-utf8.srt && \
-	my_iconv utf-16 s02-utf16.srt | diff - s02-utf8.srt  && \
-	! diff s03-ascii-bom-cr-d.out s03-ascii-d.out > /dev/null && \
-	rm_bom_cr < s03-ascii-bom-cr-d.out | diff - s03-ascii-d.out && \
-	! diff s03-ascii-bom-cr.srt s03-ascii.srt > /dev/null && \
-	rm_bom_cr < s03-ascii-bom-cr.srt | diff - s03-ascii.srt
-assert_ok $? "check fixtures"
-
-# SMI2SRT tests
+echo
+echo \# Checks SMI2SRT
 for i in s01*.smi; do 
-	../smi2srt <"$i" | diff - s01-utf8.srt
-	assert_ok $? "smi2srt <$i"
+	assert_ok "../smi2srt <\"$i\" | diff - s01-utf8.srt"
 done
 
-DIR=$(mktemp -d)
-cp s01*smi $DIR
-smi2srt $DIR/*smi 2> /dev/null
-assert_ok $? 'smi2srt s01*.smi'
+cp s01-*.smi $DIR
+assert_ok "smi2srt $DIR/s01-*smi 2> /dev/null"
 
-for i in $DIR/*srt; do
-	diff "$i" s01-utf8.srt > /dev/null
-	assert_ok $? "diff $(basename $i) s01-utf8.srt"
+for i in $DIR/s01*.srt; do
+	assert_ok "diff \"$i\" s01-utf8.srt > /dev/null"
 done 
-
-rm -rf $DIR
 
 for i in s02*.smi; do 
-	../smi2srt <"$i" | diff - s02-utf8.srt
-	assert_ok $? "smi2srt <$i"
+	assert_ok "../smi2srt <\"$i\" | diff - s02-utf8.srt"
 done
 
-DIR=$(mktemp -d)
-cp s02*smi $DIR
-smi2srt $DIR/*smi 2> /dev/null
-assert_ok $? 'smi2srt s02*.smi'
+cp s02-*.smi $DIR
+assert_ok "smi2srt $DIR/s02-*smi 2> /dev/null"
 
-for i in $DIR/*srt; do
-	diff "$i" s02-utf8.srt > /dev/null
-	assert_ok $? "diff $(basename $i) s02-utf8.srt"
+for i in $DIR/s02*.srt; do
+	assert_ok "diff \"$i\" s02-utf8.srt > /dev/null"
 done 
+
+echo
+echo \# Checks if test fixtures are valid
+assert_ok "my_iconv cp949 s01-cp949.smi | diff - s01-utf8.smi"
+assert_ok "my_iconv utf-16 s01-utf16.smi | diff - s01-utf8.smi"
+assert_ok "my_iconv utf-16 s01-utf16.smi | diff - s01-utf8.smi"
+assert_ok "my_iconv cp949 s01-cp949.srt | diff - s01-utf8.srt"
+assert_ok "my_iconv utf-16 s01-utf16.srt | diff - s01-utf8.srt"
+assert_ok "my_iconv utf-16 s01-utf16.srt | diff - s01-utf8.srt"
+assert_ok "my_iconv cp949 s02-cp949.smi | diff - s02-utf8.smi"
+assert_ok "my_iconv utf-16 s02-utf16.smi | diff - s02-utf8.smi"
+assert_ok "my_iconv utf-16 s02-utf16.smi | diff - s02-utf8.smi"
+assert_ok "my_iconv cp949 s02-cp949.srt | diff - s02-utf8.srt"
+assert_ok "my_iconv utf-16 s02-utf16.srt | diff - s02-utf8.srt"
+assert_ok "my_iconv utf-16 s02-utf16.srt | diff - s02-utf8.srt "
+assert_ok "! diff s03-ascii-bom-cr-d.out s03-ascii-d.out > /dev/null"
+assert_ok "rm_bom_cr < s03-ascii-bom-cr-d.out | diff - s03-ascii-d.out"
+assert_ok "rm_bom_cr < s03-ascii-bom-cr-m.out | diff - s03-ascii-m.out"
+assert_ok "! diff s03-ascii-bom-cr.srt s03-ascii.srt > /dev/null"
+assert_ok "rm_bom_cr < s03-ascii-bom-cr.srt | diff - s03-ascii.srt"
 
 rm -rf $DIR
 
-assert_ok 0 all
+echo 
+echo \# All done
